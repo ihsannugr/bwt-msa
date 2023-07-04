e91b1c2a8596132ae05f5994dc34d2c858fabab2
ihsannugr <ihsannug@BRIN-2HXP1T3>
2023-07-04 11:39:19 +0700

    fix finding exact-match for 1-2 character(s) of query sequence

--- findingmatch.py
+++ findingmatch.py
@@ -24,7 +24,7 @@ def find_char_from_fi(i):
             if i in list_of_index]
     return char[0]
 
-def find_exact(q, r, t, sa, fi, bi):
+def find_exact(q, r, t, sa, fi, bi, br):
     f = q[0]
     loc = []
     ind = 0
@@ -34,27 +34,29 @@ def find_exact(q, r, t, sa, fi, bi):
         print("First character (", f, ") found in location", fi[f], "of FC")
         if len(q) == 1:
             loc = [sa[i] for i in fi[f]]
-            print("Match found!")
-            print("Query location(s):", sorted(loc))
+            print("MATCHED")
+            print("\nMatched location(s):", sorted(loc))
         else:
             for i in fi[f]:
                 print(f+str(ind), ">>>>>")
                 for j in q[1:]:
                     rowfi = bi[f][rowbwt]
-                    if rowbwt >= len(fi[j]) or rowfi != fi[j][rowbwt]:
-                        print("No match found..")
-                        ind += 1
+                    # print(len(fi[j]))
+                    # if rowbwt >= len(fi[j]) or rowfi != fi[j][rowbwt]:
+                    # if rowbwt >= len(fi[j]) or rowfi not in fi[j]:
+                    if rowfi not in fi[j]:
+                        print("UNMATCHED")
                         rowbwt += 1
                         break
                     else:
-                        print("Match found!")
+                        print("MATCHED:", j)
                         loc.append(sa[i])
                         f = j
-                        ind += 1
                         rowbwt += 1
+                ind += 1
                 f = q[0]
-            print("Query location(s):", loc)
-    else: print("No match...")
+            print("\nMatched location(s):", loc)
+    else: print("There is no match found")
 
 # read BWT(T)    
 file1 = open("string-transformed.txt",'r')
@@ -77,9 +79,11 @@ fc_range = rbwt.firstCol(occur)[0]
 fc_index = build_fc_index(fc_range)
 print("FC Index: ", fc_index)
 
-# building dictionary of dictionary of character against its position in FC
+# building dictionary of character against its position in FC
 bwt_index = build_bwt_index(ref)
+bwt_ranks = rbwt.rankBwt(ref)[0]
 print("BWT Index: ", bwt_index)
+print("BWT Ranks: ", bwt_ranks)
 
 query = input('Query sequence: ')
-find_exact(query, ref, t, suffix, fc_index, bwt_index)
+find_exact(query, ref, t, suffix, fc_index, bwt_index, bwt_ranks)
